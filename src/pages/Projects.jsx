01e58de41095c44e3c35288b6d87fa2678eb213f
Hamzazaheer1 <mhamzazaheer1@gmail.com>
2023-11-29 01:27:33 +0500

    updates

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,24 +1,51 @@
-import { useState } from 'react';
-import { Gallery } from 'react-grid-gallery';
+import { useRef, useState } from 'react';
 import { photos } from '../utils/photo';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
 const Projects = () => {
+  const containerRef = useRef(null);
+  const [currentCategory, setCurrentCategory] = useState('All');
   const [images, setImages] = useState(photos);
 
-  const photo = images.map((image) => ({
-    ...image,
-    customOverlay: (
-      <div className="custom-overlay__caption">
-        <div>{image.caption}</div>
-        {image.tags &&
-          image.tags.map((t, index) => (
-            <div key={index} className="custom-overlay__tag">
-              {t.title}
-            </div>
-          ))}
-      </div>
-    ),
-  }));
+  const handleMouseEnter = (event) => {
+    const container = event.currentTarget;
+    container.scrollTo({
+      top: container.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
+
+  const handleMouseLeave = (event) => {
+    const container = event.currentTarget;
+    container.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  const handleFilter = (category) => {
+    setCurrentCategory(category);
+    switch (category) {
+      case 'All':
+        setImages(photos);
+        break;
+      case 'React':
+        setImages(
+          photos.filter((photo) => photo.title && photo.title.includes('React'))
+        );
+        break;
+      case 'Wordpress':
+        setImages(
+          photos.filter(
+            (photo) => photo.title && photo.title.includes('Wordpress')
+          )
+        );
+        break;
+      default:
+        setImages(photos);
+        break;
+    }
+  };
 
   return (
     <section className="min-h-screen h-full w-full container mx-auto pt-20">
@@ -26,18 +53,51 @@ const Projects = () => {
         My Projects
       </h1>
       <div className="w-full flex justify-center text-black dark:text-white">
-        <button className="hover:text-blue-500 capitalize px-3 py-4">
-          Home
+        <button
+          className={`hover:text-blue-500 capitalize px-3 py-4 ${
+            currentCategory === 'All' && 'font-bold'
+          }`}
+          onClick={() => handleFilter('All')}
+        >
+          ALL
         </button>
-        <button className="hover:text-blue-500 capitalize px-3 py-4">
-          App
+        <button
+          className={`hover:text-blue-500 capitalize px-3 py-4 ${
+            currentCategory === 'React' && 'font-bold'
+          }`}
+          onClick={() => handleFilter('React')}
+        >
+          React
         </button>
-        <button className="hover:text-blue-500 capitalize px-3 py-4">
-          Home
+        <button
+          className={`hover:text-blue-500 capitalize px-3 py-4 ${
+            currentCategory === 'WordPress' && 'font-bold'
+          }`}
+          onClick={() => handleFilter('Wordpress')}
+        >
+          WordPress
         </button>
       </div>
       <div className="my-4">
-        <Gallery images={photo} enableImageSelection={false} />
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+          <Masonry className="masonry_gap">
+            {images.map((image, i) => (
+              <div
+                style={{ height: image.height, overflowY: 'auto' }}
+                className=" hideScrollbar shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <img
+                  key={i}
+                  src={image.src}
+                  alt={image.alt}
+                  className="rounded-md object-fill w-full block"
+                />
+              </div>
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
       </div>
     </section>
   );
